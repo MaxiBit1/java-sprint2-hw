ecba970925d4970870e0d2226da9ba7c69f6be1d
MaxiBit1 <Maxvas324@yandex.ru>
2022-09-23 14:17:03 +0300

    Класс реализующий работу с годовым файлом

--- src/YearlyReport.java
+++ src/YearlyReport.java
@@ -3,16 +3,33 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 
+/** Класс для считывания и работы с данными с годового года
+ * @author Max Vasilyev
+ * @version 1.0
+ */
 public class YearlyReport {
 
+    /** Поле хранения данных с годового файла в виде хеш-таблицы */
     HashMap<Integer, YearData> mapDataYear = new HashMap<>();
+    /** Поле пути до годового файла */
     String path;
 
+    /**
+     * Внутренний класс данных со свойствами <b>month</b>,<b>costSpending</b> и <b>costIncome</b>
+     */
     class YearData {
+        /** Поле числа месяца */
         int month;
+        /** Поле количество трат */
         int costSpending;
+        /** Поле количетсва дорхода */
         int costIncome;
 
+        /** Конструктор - для создания объектов со свойствами
+         * @param month - число месяца
+         * @param cost - сумма
+         * @param isExpense - трата или доход
+         */
         public YearData(int month, int cost, boolean isExpense) {
             this.month = month;
             if (isExpense) {
@@ -23,6 +40,7 @@ public class YearlyReport {
         }
     }
 
+    /** Метод для получения данных и сохранение их в хеш-таблице */
     void readYearCSV() {
         path = "resources/y.2021.csv";
         String strYear = readFileContentsOrNull(path);
@@ -36,6 +54,10 @@ public class YearlyReport {
         }
     }
 
+    /** Процесс счтения файла с определенным параметром
+     * @param path - название пути до файла
+     * @return возвращает считыванный файл
+     */
     private String readFileContentsOrNull(String path) {
         try {
             return Files.readString(Path.of(path));
@@ -45,15 +67,26 @@ public class YearlyReport {
         }
     }
 
+    /** Метод получения номера месяца с определенным параметром
+     * @param indexMap - индекс хэш-таблицы
+     * @return возвращает номер месяца
+     */
     int monthNumber(int indexMap) {
         return mapDataYear.get(indexMap).month;
     }
 
-    boolean isIncome(int monthIndex) {
-        YearData yearData = mapDataYear.get(monthIndex);
+    /** Метод проверки данных на трату с определенным параметром
+     * @param mapIndex - идекс хеш-таблицы
+     * @return возвращает true если сумма дохода не равна 0
+     */
+    boolean isIncome(int mapIndex) {
+        YearData yearData = mapDataYear.get(mapIndex);
         return yearData.costIncome != 0;
     }
 
+    /** Метод проверки считки годового файла
+     * @return возвращает результат проверки
+     */
     boolean checkYear() {
         if (mapDataYear.isEmpty()) {
             System.out.println("Вы не считали данные за год");
@@ -62,19 +95,30 @@ public class YearlyReport {
         return true;
     }
 
-    int sumSpendOrIncome(int monthIndex, boolean checkMonth) {
-        YearData yearData = mapDataYear.get(monthIndex);
+    /** Метод получение суммы дохода или трат
+     * @param mapIndex - индекс хэш-таблицы
+     * @param checkMonth - трата или доход
+     * @return возвращение суммы
+     */
+    int sumSpendOrIncome(int mapIndex, boolean checkMonth) {
+        YearData yearData = mapDataYear.get(mapIndex);
         if (checkMonth) {
             return yearData.costIncome;
         }
         return yearData.costSpending;
-
     }
 
+    /** Метод получения года
+     * @return возвращает номер года
+     */
     String getNameYear() {
         return path.substring(12, 16);
     }
 
+    /** Метод получения прибыли за месяца
+     * @param montNum - номер месяца
+     * @return возвращает прибыль
+     */
     int profitYearIncome(int montNum) {
         boolean checkIsIncome;
         int a = 0;
@@ -99,7 +143,9 @@ public class YearlyReport {
         return a - b;
     }
 
-
+    /** Метод получения среднего дохода за год
+     * @return возвращает средний доход
+     */
     double meanCostIncome() {
         double sum = 0;
         for (int i = 1; i < mapDataYear.size(); i++) {
@@ -109,6 +155,9 @@ public class YearlyReport {
         return sum;
     }
 
+    /** Метод получения средней траты за год
+     * @return возвращает среднюю трату
+     */
     double meanCostSpending() {
         double sum = 0;
         for (int i = 1; i < mapDataYear.size(); i++) {
