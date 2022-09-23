96b6494d9df9b375e5e184d8af8b78c1c6c729cc
MaxiBit1 <Maxvas324@yandex.ru>
2022-09-23 14:14:42 +0300

    Класс реализующий графическую часть и взаимодействия с пользователем приложения

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
 
+/**
+ * Главный класс приложения
+ * @author Max Vasilyev
+ * @version 1.0
+ */
 public class Main {
-    static MonthlyReport monthlyReport = new MonthlyReport();
-    static YearlyReport yearlyReport = new YearlyReport();
 
+    /** Главный метод приложения, взаимодейтсвующий с пользователем */
     public static void main(String[] args) {
-        // Поехали!
         Scanner scanner = new Scanner(System.in);
         while (true) {
             showMenu();
             int command = scanner.nextInt();
             if (command == 1) {
-                monthlyReport.readMonthCSV();
+                ServiceBisnesLogic.readCSV(1);
                 System.out.println("Данные с месячных отчетов считаны");
             } else if (command == 2) {
-                yearlyReport.readYearCSV();
+                ServiceBisnesLogic.readCSV(2);
                 System.out.println("Данные с годового отчета считаны");
             } else if (command == 3) {
-                checkMonthAndYear();
+                ServiceBisnesLogic.checkMonthAndYear();
             } else if (command == 4) {
-                showMonthlyInfo();
+                ServiceBisnesLogic.showMonthlyInfo();
             } else if (command == 5) {
-                showYearlyInfo();
+                ServiceBisnesLogic.showYearlyInfo();
             } else if (command == 6) {
                 break;
             } else {
@@ -30,6 +33,7 @@ public class Main {
         }
     }
 
+    /** Метод реализации печати пунктов меню */
     public static void showMenu() {
         System.out.println("=".repeat(25));
         System.out.println("1 - Считать все месячные рассчеты");
@@ -38,65 +42,6 @@ public class Main {
         System.out.println("4 - Вывести информацию о месячных отчетах");
         System.out.println("5 - Вывести информацию о годовом отчете");
         System.out.println("6 - Выход");
-    }
-
-    public static void checkMonthAndYear() {
-        if(!monthlyReport.checkMonth() && !yearlyReport.checkYear()){
-            return;
-        }else if(!monthlyReport.checkMonth()){
-            return;
-        }else if(!yearlyReport.checkYear()){
-            return;
-        }
-        boolean isNotError = true;
-        for (int i = 1;i<yearlyReport.mapDataYear.size();i++){
-            int monthNum = yearlyReport.monthNumber(i);
-            boolean isIncomeOrSpending = yearlyReport.isIncome(i);
-            int monthCosts = monthlyReport.sumSpendOrIncome(monthNum,isIncomeOrSpending);
-            int yearCosts = yearlyReport.sumSpendOrIncome(i,isIncomeOrSpending);
-            if(yearCosts!=monthCosts){
-                System.out.println("В " + monthNum + " месяце обнаружена ошибка");
-                isNotError = false;
-            }
-        }
-        if(isNotError){
-            System.out.println("Ошибок не обнаружено");
-        }
-    }
-
-    public static void showMonthlyInfo(){
-        if(!monthlyReport.checkMonth()){
-            return;
-        }
-        for(int i = 1; i < 4;i++){
-            System.out.println("=".repeat(25));
-            System.out.println("Месяц: " + nameMonth(i));
-            monthlyReport.maxIncomeThingIncome(i);
-            monthlyReport.maxIncomeThingSpending(i);
-        }
-    }
-
-    public static void showYearlyInfo(){
-        System.out.println("=".repeat(25));
-        if(!yearlyReport.checkYear()){
-            return;
-        }
-        System.out.println(yearlyReport.getNameYear() + " год");
-        for (int i = 1; i <= 3;i++){
-            System.out.println("Прибыль за " + i + " месяц:" + yearlyReport.profitYearIncome(i));
-        }
-        System.out.println("Средний расход за все месяцы: " + yearlyReport.meanCostIncome());
-        System.out.println("Средний доход за все месяцы: " + yearlyReport.meanCostSpending());
-    }
-
-    public static String nameMonth(int month){
-        if(month == 1){
-            return "Январь";
-        }else if(month == 2){
-            return "Февраль";
-        }else {
-            return "Март";
-        }
     }
 }
 
